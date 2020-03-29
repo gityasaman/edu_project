822abdabca7718f9c957b7d147254abbb0c61faf
Yasaman <shokri.yasaman@yahoo.com>
2020-03-29 20:30:18 +0430

    new

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype = html>
+<<html>
+<head>
+
+</head>
+
+<body>
+
+<form action = "login.php">
+<input name = "username" type ="text">
+
+<input name = "pasword" type = "pasword">
+<input name = "button" type = "submit" value = "submit">
+</form>
+</body>
+
+</html>
+
+
